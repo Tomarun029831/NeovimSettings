276c2d48f8c2c7797d24df721fdaa04f5fc831b1
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-06-10 16:36:03 +0900

    ADD many LSPs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.shell = "pwsh"
+local o = vim.o
+o.shell = "pwsh"
+o.expandtab = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = 4
