67ff8ad2b58663e2df5e916b670cf6a95662e4ad
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-12-23 17:23:33 +0900

    feat(config, plugin): Add molten-nvim and new keybinds for molten

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- For Molten
+vim.keymap.set("n", "<localleader>mi", ":MoltenInit<CR>", { silent = true, desc = "Initialize Molten" })
+
+vim.keymap.set("n", "<localleader>mo", ":MoltenEvaluateOperator<CR>", { silent = true, desc = "Evaluate operator" })
+vim.keymap.set("n", "<localleader>ml", ":MoltenEvaluateLine<CR>", { silent = true, desc = "Evaluate line" })
+vim.keymap.set("n", "<localleader>mc", ":MoltenReevaluateCell<CR>", { silent = true, desc = "Re-evaluate cell" })
+
+vim.keymap.set("n", "<localleader>ms", ":MoltenSave<CR>", { silent = true, desc = "Save Molten state" })
+vim.keymap.set("n", "<localleader>mL", ":MoltenLoad<CR>", { silent = true, desc = "Load Molten state" })
+
+vim.keymap.set(
+    "v",
+    "<localleader>mv",
+    ":<C-u>MoltenEvaluateVisual<CR>gv",
+    { silent = true, desc = "Evaluate visual selection" }
+)
