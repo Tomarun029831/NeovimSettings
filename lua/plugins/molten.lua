a168656089f71ab2d79b56290de8179f5b45ca69
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-12-23 19:43:35 +0900

    chore(molten): paste config from molten-nvim docs

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -4,12 +4,12 @@ return {
 
     dependencies = "willothy/wezterm.nvim",
     init = function()
-        vim.g.molten_auto_open_output = false
+        vim.g.molten_auto_open_output = false -- cannot be true if molten_image_provider = "wezterm"
         vim.g.molten_output_show_more = true
         vim.g.molten_image_provider = "wezterm"
         vim.g.molten_output_virt_lines = true
-        vim.g.molten_split_direction = "right"
-        vim.g.molten_split_size = 40
+        vim.g.molten_split_direction = "right" --direction of the output window, options are "right", "left", "top", "bottom"
+        vim.g.molten_split_size = 40 --(0-100) % size of the screen dedicated to the output window
         vim.g.molten_virt_text_output = true
         vim.g.molten_use_border_highlights = true
         vim.g.molten_virt_lines_off_by_1 = true
